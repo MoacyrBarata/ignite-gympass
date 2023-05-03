dc2ea1aae981d47906046c382eccacd74d0f60db
Moacyr Barata <moacyrbarata@gmail.com>
2023-05-03 15:51:58 -0400

    checkin repository

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,20 @@
+import { User, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryCheckInsRepository implements CheckInsRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.CheckinUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+    this.items.push(user)
+
+    return user
+  }
+}
